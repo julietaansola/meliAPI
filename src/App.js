5e2a5015f543a85e7a8c541af030ab9fdd0da249
julieta <julietaansolaberraute@gmail.com>
2020-07-05 20:33:16 -0300

    move fetch to innercarousel

--- src/App.js
+++ src/App.js
@@ -5,29 +5,10 @@ import "./App.css";
 import Carousel from "./Carousel";
 
 class App extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			items: [],
-		};
-	}
-
-	async componentDidMount() {
-		const search = await fetch(
-			"https://api.mercadolibre.com/sites/MLA/search?q=FUNKO&limit=8"
-		);
-		const searchJson = await search.json();
-		const result = searchJson.results;
-		console.log(result);
-		this.setState({
-			items: result,
-		});
-	}
-
 	render() {
 		return (
 			<div>
-				<Carousel items={this.state.items} title="Basado en tu ultima visita" />
+				<Carousel items={this.props.items} title="Basado en tu ultima visita" />
 			</div>
 		);
 	}
--- src/InnerCarousel/index.js
+++ src/InnerCarousel/index.js
@@ -3,12 +3,30 @@ import Card from "../Card";
 import "./style.css";
 
 class InnerCarousel extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			items: [],
+		};
+	}
+
+	async componentDidMount() {
+		const search = await fetch(
+			"https://api.mercadolibre.com/sites/MLA/search?q=FUNKO&limit=8"
+		);
+		const searchJson = await search.json();
+		const result = searchJson.results;
+		console.log(result);
+		this.setState({
+			items: result,
+		});
+	}
 	render() {
 		return (
 			<div className="carousel-container">
 				<h1>{this.props.title}</h1>
 
-				<Card items={this.props.items} />
+				<Card items={this.state.items} />
 			</div>
 		);
 	}
